ccbd6b89d1d779c09ed9d22eb130bc11af36410f
edwardg <egarrett3@fordham.edu>
2020-09-01 15:27:56 -0400

    refactoring for Express

--- app.js
+++ app.js
@@ -3,9 +3,33 @@ const app = express();
 const path = require("path");
 const fetch = require("node-fetch");
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
+let d3 = require("d3");
+
 
 app.use(express.static("dist"));
 
+
 app.get("/", (request, res) => {
   res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
+
+app.get("graph/:state", (request, response) => {
+  debugger
+  // make api call using fetch
+  fetch (
+    `http://coronavirusapi.com/getTimeSeries/${state}`
+  )
+    .then((response) => {
+      return response.text();
+    })
+    .then((text) => {
+      let results = d3.csvParse(text);
+      console.log(results); // logs to server
+      response.send(results); // sends to frontend
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(__dirname);
+  console.log(`listening on ${PORT}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import './styles/index.css';
 import './styles/graph.css';
-import './scripts/graph';
-import Graph from './scripts/graph';
+import renderData from './getStateData';
+import axios from 'axios';
+import graph from './graph';
 
-var d3 = require("d3");
-
-// function getData() {
-//   return fetch(`http://coronavirusapi.com/getTimeSeries/TN`, {
-//     method: "GET",
-// }).then((response) => { return response.text()})
-//   .then(text => { return d3.csvParse(text) })
-// }
-
-// getData().then(data => { debugger; formGraph(data) });
-
-
-    
+window.addEventListener("DOMContentLoaded", () => {
+    graph();
+    // renderData("CA");
+});
